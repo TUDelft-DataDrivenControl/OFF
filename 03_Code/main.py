5486dca8c0c1a5e19a7f60812eb82a31498894f2
Marcus Becker <marcus.becker@tudelft.nl>
2022-10-10 17:33:45 -0600

    UML update & renaming 02_Examples

--- 03_Code/main.py
+++ 03_Code/main.py
@@ -36,7 +36,7 @@ def main():
                          ('cw', 20),                            # Dummy wake value
                          ('sig dw', 500),                       # Dummy wake value
                          ('sig r', 40),                         # Dummy wake value
-                         ('gch_yaml_path', '02_Examples/00_Inputs/01_FLORIS/gch.yaml')])  # Gaussian wake parameters
+                         ('gch_yaml_path', '02_Examples_and_Cases/00_Inputs/01_FLORIS/gch.yaml')])  # Gaussian wake parameters
 
     # Create turbines
     #   Turbines are created with
