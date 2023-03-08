d28b204ad25a36adda76bb1c4c50fba8ca0fdecb
Marcus Becker <marcus.becker@tudelft.nl>
2023-03-08 20:14:52 +0100

    Adapted the root dir path generation
    This needs a longterm fix, currently it breaks too frequently, even with the same OS and different IDEs

--- 03_Code/off/off.py
+++ 03_Code/off/off.py
@@ -91,7 +91,10 @@ class OFF:
             root_dir = data_dir or f'{os.environ["OFF_PATH"]}/runs/'
             lg.info('Root runs directory: ' + root_dir)
         except KeyError:
-            root_dir = data_dir or f'{os.environ["PWD"][:-len("03_Code")]}/runs/'
+            if os.environ["PWD"].endswith("03_Code"):
+                root_dir = data_dir or f'{os.environ["PWD"][:-len("03_Code")]}/runs/'
+            else:
+                root_dir = data_dir or f'{os.environ["PWD"]}/runs/'
             lg.warning('Initial root runs directory path retrieval was unsuccessful, used ' + root_dir)
 
         self.sim_dir = f'{root_dir}/off_run_{run_id}' if sim_dir is None else sim_dir
