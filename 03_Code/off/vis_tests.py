ddd94ecb231287634810979099c304bf66da4518
lejeunemax <maxime.lejeune@uclouvain.be>
2023-05-24 14:47:55 +0200

    Updating license

--- 03_Code/off/vis_tests.py
+++ 03_Code/off/vis_tests.py
@@ -1,3 +1,21 @@
+# Copyright (C) <2023>, M Becker (TUDelft), M Lejeune (UCLouvain)
+
+# List of the contributors to the development of OFF: see LICENSE file.
+# Description and complete License: see LICENSE file.
+	
+# This program (OFF) is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program (see COPYING file).  If not, see <https://www.gnu.org/licenses/>.
+
 import matplotlib.pyplot as plt
 
 # Define the x and y coordinates of the dots
