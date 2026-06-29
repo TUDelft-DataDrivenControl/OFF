dc1d72c765e93d250594f875e113fdded460a320
Marcus Becker <marcus.becker@tudelft.nl>
2026-06-29 09:07:54 +0200

    Observables Turbine Controller

--- src/TurbineModel/turbine_controller/TurbineController.py
+++ src/TurbineModel/turbine_controller/TurbineController.py
@@ -1,3 +1,4 @@
+import numpy as np
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
@@ -14,3 +15,91 @@ class TurbineController(ABC):
         """
         return "No specific Turbine Controller citation available."
 
+    """ 
+    ---------------------------------------
+    Observables 
+    --------------------------------------- 
+    """
+
+    @abstractmethod
+    def obs_power_setpoint_w(self) -> float:
+        """ Abstract method to observe the current power setpoint of the turbine.
+
+        Raises:
+            NotImplementedError: Abstract Method, must be implemented in derived classes.
+
+        Returns:
+            float: Current power setpoint of the turbine (W).
+        """
+        raise NotImplementedError
+    
+    @abstractmethod
+    def obs_yaw_setpoint_deg(self) -> float:
+        """ Observes the current yaw setpoint of the turbine.
+
+        Raises:
+            NotImplementedError: Abstract Method, must be implemented in derived classes.
+
+        Returns:
+            float: Current yaw setpoint of the turbine (deg).
+        """
+        raise NotImplementedError
+    
+    @abstractmethod
+    def obs_pitch_setpoint_deg(self) -> float:
+        """ Observes the current pitch setpoint of the turbine.
+
+        Raises:
+            NotImplementedError: Abstract Method, must be implemented in derived classes.
+
+        Returns:
+            float: Current pitch setpoint of the turbine (deg).
+        """
+        raise NotImplementedError
+    
+    @abstractmethod
+    def obs_torque_setpoint_nm(self) -> float:
+        """ Observes the current torque setpoint of the turbine.
+
+        Raises:
+            NotImplementedError: Abstract Method, must be implemented in derived classes.
+
+        Returns:
+            float: Current torque setpoint of the turbine (Nm).
+        """
+        raise NotImplementedError
+    
+    @abstractmethod
+    def obs_rotor_speed_setpoint_radps(self) -> float:
+        """ Observes the current rotor speed setpoint of the turbine.
+
+        Raises:
+            NotImplementedError: Abstract Method, must be implemented in derived classes.
+
+        Returns:
+            float: Current rotor speed setpoint of the turbine (rpm).
+        """
+        raise NotImplementedError
+    
+    def obs_rotor_speed_setpoint_rpm(self) -> float:
+        """ Observes the current rotor speed setpoint of the turbine.
+
+        Raises:
+            NotImplementedError: Abstract Method, must be implemented in derived classes.
+
+        Returns:
+            float: Current rotor speed setpoint of the turbine (rpm).
+        """
+        return self.obs_rotor_speed_setpoint_radps() * 60 / (2 * np.pi)
+
+    @abstractmethod
+    def obs_curtailment_factor(self) -> float:
+        """ Observes the current curtailment factor of the turbine. 0 means no curtailment, 1 means full curtailment.
+
+        Raises:
+            NotImplementedError: Abstract Method, must be implemented in derived classes.
+
+        Returns:
+            float: Current curtailment factor of the turbine (dimensionless).
+        """
+        raise NotImplementedError
