455352020ac3a19667d4c11296286871ec68706b
Marcus Becker <marcus.becker@tudelft.nl>
2026-07-01 11:17:33 +0200

    Example how you can run an incremental simulation and adapt the control setpoints using the YawSteeringPrescribedMotionController

---
+++ 03_Code/main_008_Incremental_Simulation_yaw_setpoints.py
@@ -0,0 +1,168 @@
+# //////////////////////////////////////////////////////////////////// #
+#    ____  ______ ______ 
+#   / __ \|  ____|  ____|
+#  | |  | | |__  | |__   
+#  | |  | |  __| |  __|  
+#  | |__| | |    | |     
+#   \____/|_|    |_|     
+# //////////////////////////////////////////////////////////////////// #
+
+# Copyright (C) <2024>, M Becker (TUDelft), M Lejeune (UCLouvain)
+
+# List of the contributors to the development of OFF: see LICENSE file.
+# Description and complete License: see LICENSE file.
+	
+# This program (OFF) is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program (see COPYING file).  If not, see <https://www.gnu.org/licenses/>.
+
+# //////////////////////////////////////////////////////////////////// #
+# Welcome to the example OFF main file. This showcases how to run a simulation using the OFF framework.
+# The settings are defined in the run_example.yaml file, have a look to see what is possible.
+# If you experience issues, create a new issue on the GitHub page https://github.com/TUDelft-DataDrivenControl/OFF
+# //////////////////////////////////////////////////////////////////// #
+
+import os, logging
+logging.basicConfig(level=logging.ERROR)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import off.off as off
+import off.off_interface as offi
+import time
+from pathlib import Path
+OFF_PATH: Path = Path(off.OFF_PATH)
+
+# This example shows how to run a simulation using the OFF framework in an incremental manner.
+# In addition, it updates the yaw setpoints of the turbines at each incremental step, showcasing how to change control inputs during an incremental simulation.
+
+def main():
+    # ======================================================= #
+    # Run incremental simulation
+    # ======================================================= #
+    start_time = time.time()
+
+    Delta_t = 400.0 # seconds
+    # Create an interface object
+    oi_inc = offi.OFFInterface()
+    
+    # Initialize the simulation with the same settings as the continous simulation
+    oi_inc.init_simulation_by_path( OFF_PATH / "02_Examples_and_Cases" / "02_Example_Cases" / "001_two_turbines_yaw_step_pywake.yaml")
+    
+    # Retrieve the simulation end time from the settings of the continous simulation
+    sim_end_time = oi_inc.off_sim.settings_sim['time end']
+
+    # Create placeholder for incremental measurements and control
+    measurements_inc = pd.DataFrame()
+    control_inc      = pd.DataFrame()
+
+    # create a deep copy of the initial control data to use for plotting later
+    control_data_initial = oi_inc.settings_ctr.copy()
+
+    for step in np.arange(0.0, sim_end_time, Delta_t):
+        # Update the total number of iterations for the incremental simulation (only needed for progress bar)
+        oi_inc.off_sim.iterations_total = int(Delta_t / oi_inc.off_sim.settings_sim['time step']) + 1
+
+        # Update the yaw setpoints of the turbines 
+        # Get state of the simulation at the current step
+        current_state = oi_inc.get_state()
+
+        print(f"Current lut at step {step}:\n{current_state.controller.lut}")
+        print(f"Current t at step {step}:\n{current_state.controller.t}")
+
+        orientation_current = [np.interp(step, current_state.controller.t, current_state.controller.lut[:, 0]),
+                                 np.interp(step, current_state.controller.t, current_state.controller.lut[:, 1])]
+        
+        print(f"Current orientation at step {step}:\n{orientation_current}")
+
+        # Generate a random sign +-1
+        r_sign = np.random.choice([-1, 1])
+
+        # Fill control_data with 4 equal rows of the current orientation
+        current_state.controller.lut = np.tile(orientation_current, (4, 1))
+        current_state.controller.lut[2][0] += r_sign * 5.0 # Change the yaw of turbine 1 by +-5 degrees randomly
+        current_state.controller.lut[3][0] += r_sign * 5.0 # Change the yaw of turbine 2 by +-5 degrees randomly
+
+        current_state.controller.t   = [step, step + Delta_t/2, step + Delta_t/2 +15.0, sim_end_time]
+        print(f"New lut at step {step}:\n{current_state.controller.lut}")
+        print(f"New t at step {step}:\n{current_state.controller.t}")
+
+        oi_inc.set_state(current_state)
+
+        # Run the incremental simulation for Delta_t seconds
+        oi_inc.increment_sim(Delta_t-oi_inc.off_sim.settings_sim['time step'], start_time=step)
+
+        # Get output and concatenate to the previous outputs
+        measurements_inc = pd.concat([measurements_inc, oi_inc.get_measurements()])
+        control_inc      = pd.concat([control_inc, oi_inc.get_applied_control()])
+
+    print("\n---OFF Incremental Simulation took %s seconds ---" % (time.time() - start_time))
+
+
+
+
+    # ======================================================= #
+    # Plot applied control and measurements compared to initial control 
+    # ======================================================= #
+    fig, axs = plt.subplots(2, 1, figsize=(12, 6), sharex=True)
+    axs[0].plot(measurements_inc[(measurements_inc['t_idx'] == 0)]['time'],
+                 measurements_inc[(measurements_inc['t_idx'] == 0)]['Power_PyWake']/1e6, 
+                 color='orange', linestyle='-', label='Power T1')
+    axs[0].vlines(np.arange(0, sim_end_time, Delta_t),
+                color='gray', linestyle=':', alpha=0.5,
+                ymin=0, ymax=1.1*np.max(measurements_inc['Power_PyWake'])/1e6,
+                label='Incremental Simulation Steps')
+    axs[0].plot(measurements_inc[(measurements_inc['t_idx'] == 1)]['time'],
+                 measurements_inc[(measurements_inc['t_idx'] == 1)]['Power_PyWake']/1e6, 
+                 color='orange', linestyle='-', label='Power T2')
+    axs[0].set_ylim(0, 1.05*np.max(measurements_inc['Power_PyWake'])/1e6)
+    axs[0].set_ylabel('Power (MW)')
+    axs[0].set_xlim(0, sim_end_time)
+    axs[0].set_xlabel('Time (s)')
+    axs[0].set_title('Turbine 0 & Turbine 1 Power Comparison')
+    axs[0].legend()
+    
+
+    # Plot Yaw comparison
+    axs[1].plot(control_inc[(control_inc['t_idx'] == 0)]['time'],
+                 control_inc[(control_inc['t_idx'] == 0)]['yaw'], 
+                 color='orange', linestyle='-', label='T1 applied yaw')
+    axs[1].plot(control_inc[(control_inc['t_idx'] == 1)]['time'],
+                 control_inc[(control_inc['t_idx'] == 1)]['yaw'], 
+                 color='orange', linestyle='-', label='T2 applied yaw')
+    axs[1]. plot(control_data_initial["orientation_t"],
+                 [270 - row[0] for row in control_data_initial["orientation_deg"]],
+                 color='blue', linestyle='--', label='T1 initial yaw')
+    axs[1]. plot(control_data_initial["orientation_t"],
+                 [270 - row[1] for row in control_data_initial["orientation_deg"]],
+                    color='blue', linestyle='--', label='T2 initial yaw')
+    axs[1].vlines(np.arange(0, sim_end_time, Delta_t),
+                color='gray', linestyle=':', alpha=0.5,
+                ymin=-32.5, ymax=32.5,
+                label='Incremental Simulation Steps')
+
+
+    axs[1].set_ylim(-32.5, 32.5)
+    axs[1].set_ylabel('Yaw (deg)')
+    axs[0].set_xlim(0, sim_end_time)
+    axs[0].set_xlabel('Time (s)')
+    axs[1].set_title('Turbine 0 & Turbine 1 Yaw Comparison')
+    axs[1].set_xlabel('Time (s)')
+    axs[1].legend()
+    plt.tight_layout()
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
